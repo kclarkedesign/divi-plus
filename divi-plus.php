664c6c645abdab8834279641c86c796ee313c2bd
Kirk Clarke <kclarke.design@gmail.com>
2022-01-22 23:15:05 -0500

    fix typo in main plugin file

--- divi-plus.php
+++ divi-plus.php
@@ -22,7 +22,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with y. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+along with Divi Plus. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
 
 
